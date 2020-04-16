2022e154f442097683ce96db6cedfde4194aba9f
darh0ky <jgamerino@gmail.com>
2020-04-16 08:51:17 -0400

    deploy

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.use(express.static(__dirname + "/public"));
 app.use("/", router);
 
 
-// Start server
-app.listen(PORT, function() {
-	console.log("Listening on PORT " + PORT);
-});
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
