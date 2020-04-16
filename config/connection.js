61c0e0d17c8a8f413d2bd1646efb0bcfd5035eec
darh0ky <jgamerino@gmail.com>
2020-04-15 20:36:30 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,10 @@ if (process.env.JAWSDB_URL) {
     // Database is local
     connection = mysql.createConnection({
         port: 3306,
-        host: 'lzy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'to5a3myy4iew4m7w',
-        password: 'r0d4b0d94i6zr0ug',
-        database: 'bh22f0c1919hyoxsu'
+        host: 'localhost',
+        user: 'root',
+        password: 'testtest',
+        database: 'burgers_db'
     })
 };
 
