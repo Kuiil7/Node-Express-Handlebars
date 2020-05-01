da50e258711028b8011aba3e733404e6a5dc0f46
darh0ky <jgamerino@gamil.com>
2020-05-01 18:27:59 -0400

    add

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
 // Set up MySQL connection.
+require("dotenv").config();
 var mysql = require("mysql");
-var app = require('../server');
 
-var connection;
-if (process.env.JAWSDB_URL){
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   var connection = mysql.createConnection({
-    port: 3306,
     host: "localhost",
+    port: process.env.PORT || 3306,
     user: "root",
-    password: "testtest",
+    password: process.env.PASS,
     database: "burgers_db"
   });
-}
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
